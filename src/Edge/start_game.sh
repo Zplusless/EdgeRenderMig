d2a74f2e1e4752e3a1caec7b8b07e10b2c71b3c4
zplusless <liyuanzhe351@163.com>
2021-11-09 21:44:18 +0800

    edge端直接用ga的periodic模式默认的desktop配置文件

--- src/Edge/start_game.sh
+++ src/Edge/start_game.sh
@@ -1,1 +1,2 @@
-cd /home/lyz/gaminganywhere-master/bin && ./ga-server-periodic config/server.snake.conf
+# cd /home/lyz/gaminganywhere-master/bin && ./ga-server-periodic config/server.snake.conf
+cd /home/lyz/gaminganywhere-master/bin && ./ga-server-periodic config/server.desktop.conf
